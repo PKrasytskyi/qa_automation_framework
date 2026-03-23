12d7be8ddd449f626cfa8497d4181d2fe5361dab
Pkrasytskyi <pkrasytskyi@gmail.com>
2026-03-23 12:20:31 +0100

    feat(auth): implement login automation with DataProvider
    - added MainPage, LoginPage, SecurePage (Page Object Model)
    - implemented positive and negative login scenarios
    - added logout test case
    - integrated DataProvider for test data

---
+++ src/main/java/data/UserLoginData.java
@@ -0,0 +1,20 @@
+package data;
+
+import org.testng.annotations.DataProvider;
+
+public class UserLoginData {
+
+    @DataProvider(name = "validUserData")
+    public Object[][] validData(){
+        return new Object[][]{
+                {"tomsmith", "SuperSecretPassword"}
+        };
+    }
+
+    @DataProvider(name = "invalidUserData")
+    public Object[][] invalidData(){
+        return new Object[][]{
+                {"tomsmith", "wrongSuperSecretPassword"}
+        };
+    }
+}
